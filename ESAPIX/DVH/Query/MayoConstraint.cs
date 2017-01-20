bb2627cc1b981552bf30f7349bafcaf058d2be1d
rexcardan <rcardan@uabmc.edu>
2017-01-20 10:10:31 -0600

    Added Mayo constraint parsing and writing

--- ESAPIX/DVH/Query/MayoConstraint.cs
+++ ESAPIX/DVH/Query/MayoConstraint.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ESAPIX.Extensions;
 
 namespace ESAPIX.DVH.Query
 {
@@ -11,5 +12,37 @@ namespace ESAPIX.DVH.Query
         public MayoQuery Query { get; set; }
         public Discriminator Discriminator { get; set; }
         public double ConstraintValue { get; set; }
+
+        /// <summary>
+        /// Reads a constraint of the form {MayoQuery} {Discriminator} {ConstraintValue}
+        /// </summary>
+        /// <param name="constraint"></param>
+        /// <returns></returns>
+        public static MayoConstraint Read(string constraint)
+        {
+            var split = constraint.SplitOnWhiteSpace();
+            if (split.Length != 3) { throw new ArgumentException("Mayo constraints much be 3 parts separated by whitespace : {MayoQuery} {Discriminator} {ConstraintValue}"); }
+            var mq = MayoQuery.Read(split[0]);
+            var discrimator = DiscriminatorConverter.ReadDiscrimator(split[1]);
+            var constraintValue = double.Parse(split[3]);
+            return new MayoConstraint()
+            {
+                Query = mq,
+                Discriminator = discrimator,
+                ConstraintValue = constraintValue
+            };
+        }
+
+        /// <summary>
+        /// Writes a constraint of the form {MayoQuery} {Discriminator} {ConstraintValue}
+        /// </summary>
+        /// <returns>a string of the constraint</returns>
+        public string Write()
+        {
+            var query = MayoQueryWriter.Write(Query);
+            var discriminator = DiscriminatorConverter.WriteDiscrimator(this.Discriminator);
+            var constraintValue = this.ConstraintValue.ToString("F2");
+            return $"{query} {discriminator} {constraintValue}";
+        }
     }
 }
---
+++ ESAPIX/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ESAPIX.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string[] SplitOnWhiteSpace(this string s)
+        {
+            return Regex.Split(s, @"\s+").Where(r => !string.IsNullOrEmpty(r)).ToArray();
+        }
+    }
+}
