6494ef875fe0dc6ba7d9ab38ee10727202380b81
Rex Cardan <rexcardan@gmail.com>
2018-07-24 17:54:56 -0500

    AppBootstrapper now has property to disable patient selection. Also starts Window at Maximized state.

--- ESAPIX.Bootstrapper/AppBootstrapper.cs
+++ ESAPIX.Bootstrapper/AppBootstrapper.cs
@@ -17,6 +17,7 @@ namespace ESAPIX.Bootstrapper
 {
     public class AppBootstrapper<T> : BootstrapperBase<T> where T : Window
     {
+        public bool IsPatientSelectionEnabled { get; set; } = true;
 
         /// <summary>
         /// Constructs a bootstrapper for standalone applications from a username, password
@@ -49,7 +50,7 @@ namespace ESAPIX.Bootstrapper
         protected override void OnContentRendered(Window shell)
         {
             base.OnContentRendered(shell);
-            if (shell != null)
+            if (shell != null && IsPatientSelectionEnabled)
             {
                 var sac = _ctx as StandAloneContext;
                 var currentContent = (UIElement)shell.Content;
@@ -64,6 +65,7 @@ namespace ESAPIX.Bootstrapper
                 stackPanel.Children.Add(currentContent);
                 DockPanel.SetDock(selectPatContent, Dock.Top);
                 DockPanel.SetDock(currentContent, Dock.Top);
+                shell.WindowState = WindowState.Maximized;
                 shell.Content = stackPanel;
             }
         }
@@ -76,83 +78,6 @@ namespace ESAPIX.Bootstrapper
             {
                 (_ctx as StandAloneContext).Dispose();
                 Application.Current.Shutdown();
-            }
-        }
-
-        public void Run(string[] commandLineArgs)
-        {
-            var sac = _ctx as StandAloneContext;
-            string patientId = ArgumentParser.GetPatientId(commandLineArgs);
-            if (!string.IsNullOrEmpty(patientId))
-            {
-                sac.SetPatient(patientId);
-            }
-
-            string courseId = ArgumentParser.GetCourseId(commandLineArgs);
-            if (!string.IsNullOrEmpty(courseId))
-            {
-                var course = sac.Patient.Courses.FirstOrDefault(c => c.Id == courseId);
-                sac.SetCourse(course);
-
-                //Only do this if course is set
-                var planUid = ArgumentParser.GetPlanSetup(commandLineArgs);
-                if (!string.IsNullOrEmpty(planUid))
-                {
-                    var plan = sac.Course.PlanSetups.FirstOrDefault(p => p.UID == planUid);
-                    sac.SetPlanSetup(plan);
-                }
-
-#if !VMS110
-                var externalPlanUid = ArgumentParser.GetExternalPlanSetup(commandLineArgs);
-                if (!string.IsNullOrEmpty(planUid))
-                {
-                    var plan = sac.Course.ExternalPlanSetups.FirstOrDefault(p => p.UID == externalPlanUid);
-                    sac.SetExternalPlanSetup(plan);
-                }
-
-                var brachyPlanUid = ArgumentParser.GetBrachyPlanSetup(commandLineArgs);
-                if (!string.IsNullOrEmpty(planUid))
-                {
-                    var plan = sac.Course.BrachyPlanSetups.FirstOrDefault(p => p.UID == brachyPlanUid);
-                    sac.SetBrachyPlanSetup(plan);
-                }
-
-                var explansInScope = ArgumentParser.GetExternalPlansInScope(commandLineArgs);
-                if (explansInScope != null && explansInScope.Any())
-                {
-                    var plans = sac.Course.ExternalPlanSetups.Where(p => explansInScope.Contains(p.UID));
-                    sac.SetExternalPlansInScope(plans);
-                }
-
-                var brachyInScope = ArgumentParser.GetBrachyPlansInScope(commandLineArgs);
-                if (brachyInScope != null && brachyInScope.Any())
-                {
-                    var plans = sac.Course.BrachyPlanSetups.Where(p => explansInScope.Contains(p.UID));
-                    sac.SetBrachyPlansInScope(plans);
-                }
-#endif
-                var plansInScope = ArgumentParser.GetPlansInScope(commandLineArgs);
-                if (plansInScope != null && plansInScope.Any())
-                {
-                    var plans = sac.Course.PlanSetups.Where(p => plansInScope.Contains(p.UID));
-                    sac.SetPlansInScope(plans);
-                }
-
-#if (VMS150 || VMS151 || VMS155)
-                var ionPlansInScope = ArgumentParser.GetIonPlansInScope(commandLineArgs);
-                if (ionPlansInScope != null && ionPlansInScope.Any())
-                {
-                    var plans = sac.Course.IonPlanSetups.Where(p => explansInScope.Contains(p.UID));
-                    sac.SetIonPlansInScope(plans);
-                }
-
-                var ionPlanUid = ArgumentParser.GetIonPlanSetup(commandLineArgs);
-                if (!string.IsNullOrEmpty(planUid))
-                {
-                    var plan = sac.Course.IonPlanSetups.FirstOrDefault(p => p.UID == externalPlanUid);
-                    sac.SetIonPlanSetup(plan);
-                }
-#endif
             }
         }
     }
