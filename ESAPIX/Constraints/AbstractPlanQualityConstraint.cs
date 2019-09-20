53c9d4fe705353fde362f68a7bb7ad067d6ea54d
Rex Cardan <rexcardan@gmail.com>
2019-09-20 10:58:49 -0500

    Added the ability for custom settings for plan constraints

--- ESAPIX/Constraints/AbstractPlanQualityConstraint.cs
+++ ESAPIX/Constraints/AbstractPlanQualityConstraint.cs
@@ -1,5 +1,8 @@
 ﻿#region
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using VMS.TPS.Common.Model.API;
 
 #endregion
@@ -10,6 +13,10 @@ namespace ESAPIX.Constraints
     {
         public delegate void StatusUpdateHandler(double progress, string status);
 
+        /// <summary>
+        /// Optional parameter to set the "value" returned of a particular constraint. Can be
+        /// used for reporting
+        /// </summary>
         public virtual string Value { get; protected set; }
         public abstract string Name { get; }
 
@@ -27,6 +34,32 @@ namespace ESAPIX.Constraints
         public void OnStatusChanged(double progress, string status)
         {
             StatusChanged?.Invoke(progress, status);
+        }
+
+        /// <summary>
+        /// A list of custom settings appropriate for the constraint implemented. Use ValidateSettings
+        /// method to ensure setting strings are correct
+        /// </summary>
+        public Dictionary<string, string> ConstraintSettings { get; set; }
+            = new Dictionary<string, string>();
+
+        /// <summary>
+        /// A method to validate the settings values to make sure they are appropriate.
+        /// This will be called when settings are changed. 
+        /// </summary>
+        /// <returns>Returns a dictionary of keys matching the settings keys with validation
+        /// results for each key</returns>
+        public virtual Dictionary<string, ValidationResult> ValidateSettings()
+        {
+            if (!ConstraintSettings.Any())
+            {
+                return new Dictionary<string, ValidationResult>();
+            }
+            else
+            {
+                throw new NotImplementedException("Must override ValidateSettings method if " +
+                    "settings feature is used.");
+            }
         }
     }
 }
---
+++ ESAPIX/Constraints/ValidationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESAPIX.Constraints
+{
+    public struct ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
